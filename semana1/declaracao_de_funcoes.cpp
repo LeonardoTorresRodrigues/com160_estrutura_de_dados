68fc1338ea36f9ff96c81bb2a7ed498a7e7abfa2
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2026-02-07 18:49:52 -0300

    feat: aula 1 introdução ao C++

---
+++ semana1/declaracao_de_funcoes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int sum(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+int sub(int number1, int number2)
+{
+    return number1 - number2;
+}
+
+int mul(int number1, int number2)
+{
+    return number1 * number2;
+}
+
+int idiv(int number1, int number2)
+{
+    return number1 / number2;
+}
+
+int main()
+{
+}
---
+++ semana1/operacoes_condicionais.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int number1;
+    int number2;
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> number1;
+    std::cout << "Digite o segundo número: ";
+    std::cin >> number2;
+
+    if (number1 == number2)
+        std::cout << number1 << " == " << number2 << std::endl;
+    if (number1 != number2)
+        std::cout << number1 << " != " << number2 << std::endl;
+    if (number1 < number2)
+        std::cout << number1 << " < " << number2 << std::endl;
+    if (number1 > number2)
+        std::cout << number1 << " > " << number2 << std::endl;
+}
