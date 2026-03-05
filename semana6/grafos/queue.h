8a7e49c10ad4e4a5a7286101dc9222e288f093ed
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2026-03-05 18:22:52 -0300

    feat: semana 6: implementando matriz de grafos

---
+++ semana6/grafos/queue.h
@@ -0,0 +1,23 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "node_type.h"
+
+class Queue
+{
+public:
+    Queue();  // Constructor
+    ~Queue(); // Destrutor
+    bool isEmpty() const;
+    bool isFull() const;
+    void print() const;
+
+    void enqueue(ItemType);
+    ItemType dequeue();
+
+private:
+    NodeType *front;
+    NodeType *rear;
+};
+
+#endif
